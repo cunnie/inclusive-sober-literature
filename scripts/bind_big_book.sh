14a25de76de600105211a437ae28d27d9509b131
Tom Dunlap <tom@motevets.com>
2020-06-14 19:46:11 -0400

    Move CSS and build scripts into inclusive-sober-literature

---
+++ scripts/bind_big_book.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -eu
+
+BOOK_TITLE="big_book"
+output_dir=$(mktemp -d)
+output_html="${output_dir}/${BOOK_TITLE}.html"
+output_pdf="${output_dir}/${BOOK_TITLE}.pdf"
+
+cd alcobinder
+
+go run cmd/alcobinder/main.go ../remixed/big_book/ ../css/big-book.css $output_html
+weasyprint --presentational-hints $output_html $output_pdf
+echo $output_html
+echo $output_pdf
+xdg-open $output_pdf
